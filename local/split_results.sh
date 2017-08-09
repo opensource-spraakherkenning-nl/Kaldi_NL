a42a0cc6a03acf26e4c09d2f68ae11be19ef330a
laurensw75 <laurensw75@gmail.com>
2017-08-09 17:01:14 +0200

    Update split_results.sh

--- local/split_results.sh
+++ local/split_results.sh
@@ -1,18 +1,18 @@
 cat $1 | grep -a 'MS\|FS\|MT\|FT' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "A : %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' >$2	
+	awk 'END { printf "A : %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' >$2	
 cat $1 | grep -a 'MS\|MT' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "M : %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
+	awk 'END { printf "M : %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
 cat $1 | grep -a 'FS\|FT' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "F : %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
+	awk 'END { printf "F : %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
 cat $1 | grep -a 'MS\|FS' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "S : %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
+	awk 'END { printf "S : %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
 cat $1 | grep -a 'MS' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "MS: %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
+	awk 'END { printf "MS: %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
 cat $1 | grep -a 'FS' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "FS: %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
+	awk 'END { printf "FS: %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
 cat $1 | grep -a 'MT\|FT' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "T : %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2
+	awk 'END { printf "T : %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2
 cat $1 | grep -a 'MT' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "MT: %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
+	awk 'END { printf "MT: %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2	
 cat $1 | grep -a 'FT' | awk '{printf "%7d %7d\n", $5, ($5*$11/100)+0.5}' | \
-	awk 'END { printf "FT: %7d %7d - %5.2f % WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2
+	awk 'END { printf "FT: %7d %7d - %5.2f %% WER\n", s, e, e/s*100 } { s += $1; e += $2 }' 2>/dev/null >>$2
