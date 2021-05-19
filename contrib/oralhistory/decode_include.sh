80b8b629f27e1c0b75ead82ae9cb76c2f57f1453
Maarten van Gompel <proycon@anaproy.nl>
2021-05-19 14:36:22 +0200

    added postprocessing steps that were part of the oral history service directly to the main script.

--- contrib/oralhistory/decode_include.sh
+++ contrib/oralhistory/decode_include.sh
@@ -47,7 +47,10 @@
 set -a
 
 die() {
+	echo "-------------------------------------------------" >&2
     echo "ERROR: $@" >&2
+	echo "-------------------------------------------------" >&2
+	[ ! -n "$logging" ] && echo "(you can inspect the log at $logging for details)" >&2
     exit 2
 }
 
@@ -100,6 +103,12 @@ if [ $# -lt 2 ]; then
 fi
 
 result=${!#}
+if [[ "$model" = *" "* ]]; then
+	die "Model path ($model) may not contain any spaces!"
+fi
+if [[ "$result" = *" "* ]]; then
+	die "Output path ($result) may not contain any spaces!"
+fi
 inter="${result}/intermediate"
 data="${inter}/data"
 logging="${inter}/log"
@@ -136,9 +145,9 @@ fi
 
 ## feature generation
 if [ $stage -le 5 ]; then
-    echo -e "\n==========================">&2
+	echo -e "\n==========================">&2
 	echo "Feature generation" | tee $inter/stage >&2
-    echo "==========================">&2
+	echo "==========================">&2
 	[ -e $model/mfcc.conf ] && cp $model/mfcc.conf $inter 2>/dev/null
 	[ -e $model/conf/mfcc.conf ] && cp $model/conf/mfcc.conf $inter 2>/dev/null
 	steps/make_mfcc.sh --nj $this_nj --mfcc-config $inter/mfcc.conf $data/ALL $data/ALL/log $inter/mfcc | tee -a $logging >&2
@@ -147,9 +156,9 @@ fi
 
 ## decode
 if [ $stage -le 6 ]; then
-    echo -e "\n==========================">&2
+	echo -e "\n==========================">&2
 	echo "Decoding" | tee $inter/stage >&2
-    echo "==========================">&2
+	echo "==========================">&2
 	echo -n "Duration of speech: " >&2
 	cat $data/ALL/segments | awk '{s+=$4-$3} END {printf("%.0f", s)}' | local/convert_time.sh
 	totallines=$(cat $data/ALL/segments | wc -l)
@@ -159,16 +168,16 @@ if [ $stage -le 6 ]; then
 	cp -r models/AM/conf models/AM/final.mdl models/AM/frame_subsampling_factor $tmp_decode
 
 
-    echo "Running decoder, this may take a long time...." | tee -a $logging >&2
-    failed=0
-    eval $timer steps/online/nnet3/decode.sh --nj $this_nj --acwt 1.2 --post-decode-acwt 10.0 --skip-scoring true $graph $data/ALL $tmp | tee -a $logging >&2 || failed=1  #the return code will usually be 0 even if things go wrong, because a job system may be in the way
+	echo "Running decoder, this may take a long time...." | tee -a $logging >&2
+	failed=0
+	eval $timer steps/online/nnet3/decode.sh --nj $this_nj --acwt 1.2 --post-decode-acwt 10.0 --skip-scoring true $graph $data/ALL $tmp | tee -a $logging >&2 || failed=1  #the return code will usually be 0 even if things go wrong, because a job system may be in the way
 	mv -f $tmp ${inter}/decode
-    if [ ! -e "${inter}/decode/lat.${this_nj}.gz" ] || [ $failed -eq 1 ]; then
-        echo -e "NNET3 DECODING FAILED! Log follows:\n===========================\nNNET 3 DECODE LOG\n=========================" >&2
-        cat ${inter}/decode/log/decode.${this_nj}.log >&2
-        tail -1 $inter/time.log | awk '{printf( "NNet3 decoding *FAILED* in %d:%02d:%02d (CPU: %d:%02d:%02d), Memory used: %d MB                \n", int($1/3600), int($1%3600/60), int($1%3600%60), int(($2+$3)/3600), int(($2+$3)%3600/60), int(($2+$3)%3600%60), $4/1000) }'
-        die "Decoding failed, inspect decode log above"
-    fi
+	if [ ! -e "${inter}/decode/lat.${this_nj}.gz" ] || [ $failed -eq 1 ]; then
+		echo -e "NNET3 DECODING FAILED! Log follows:\n===========================\nNNET 3 DECODE LOG\n=========================" >&2
+		cat ${inter}/decode/log/decode.${this_nj}.log >&2
+		tail -1 $inter/time.log | awk '{printf( "NNet3 decoding *FAILED* in %d:%02d:%02d (CPU: %d:%02d:%02d), Memory used: %d MB                \n", int($1/3600), int($1%3600/60), int($1%3600%60), int(($2+$3)/3600), int(($2+$3)%3600/60), int(($2+$3)%3600%60), $4/1000) }'
+		die "Decoding failed, inspect decode log above"
+	fi
 	tail -1 $inter/time.log | awk '{printf( "NNet3 decoding completed in %d:%02d:%02d (CPU: %d:%02d:%02d), Memory used: %d MB                \n", int($1/3600), int($1%3600/60), int($1%3600%60), int(($2+$3)/3600), int(($2+$3)%3600/60), int(($2+$3)%3600%60), $4/1000) }'
 
 	rm -r $tmp_decode
@@ -177,19 +186,19 @@ fi
 
 ## rescore
 if [ $stage -le 7 ] && [ $llmodel ] && [ -e $inter/decode/num_jobs ]; then
-    echo -e "\n==========================">&2
+	echo -e "\n==========================">&2
 	echo "Rescoring" | tee $inter/stage >&2
-    echo "==========================">&2
+	echo "==========================">&2
 	numjobs=$(< $inter/decode/num_jobs)
 	eval $timer steps/lmrescore_const_arpa.sh --skip-scoring true $lpath $llpath $data/ALL $inter/decode $inter/rescore | tee -a $logging 2>&1 &
 	pid=$!
 	spin='-\|/'
 	i=0
- 	while kill -0 $pid 2>/dev/null; do
- 		i=$(( (i+1) %4 ))
-   		printf "\rRescoring.. ${spin:$i:1}"
-   		sleep .2
- 	done
+	while kill -0 $pid 2>/dev/null; do
+		i=$(( (i+1) %4 ))
+		printf "\rRescoring.. ${spin:$i:1}"
+		sleep .2
+	done
 	cat $inter/time.log | awk '{printf("\rRescoring completed in %d:%02d:%02d (CPU: %d:%02d:%02d), Memory used: %d MB                \n", int($1/3600), int($1%3600/60), int($1%3600%60), int(($2+$3)/3600), int(($2+$3)%3600/60), int(($2+$3)%3600%60), $4/1000) }'
 	rescore=$inter/rescore
 fi
@@ -198,15 +207,15 @@ fi
 
 ## create readable output
 if [ $stage -le 8 ] && [ -e $rescore/num_jobs ]; then
-    echo -e "\n==========================">&2
+	echo -e "\n==========================">&2
 	echo -e "Producing output" | tee $inter/stage >&2
-    echo "==========================">&2
+	echo "==========================">&2
 
 	frame_shift_opt=
 	rm -f $data/ALL/1Best.* $result/1Best* $rescore/1Best.*
 
 	numjobs=$(< $rescore/num_jobs)
-    [ -z "$numjobs" ] && numjobs=1
+	[ -z "$numjobs" ] && numjobs=1
 
 	if [ -f $model/frame_shift ]; then
 		frame_shift_opt="--frame-shift=$(cat $model/frame_shift)"
@@ -244,9 +253,9 @@ fi
 
 ## score if reference transcription exists
 if [ $stage -le 9 ] && [ -s $data/ALL/ref.stm ]; then
-    echo -e "\n==========================">&2
+	echo -e "\n==========================">&2
 	echo -e "Scoring" | tee $inter/stage >&2
-    echo "==========================">&2
+	echo "==========================">&2
 	# score using asclite, then produce alignments and reports using sclite
 	[ -s $data/ALL/test.uem ] && uem="-uem $data/ALL/test.uem"
 	for iac in $inv_acoustic_scale; do
@@ -259,11 +268,66 @@ if [ $stage -le 9 ] && [ -s $data/ALL/ref.stm ]; then
 			[ -e $result/1Best.${ident}ctm.sys ] && [ $(cat $result/1Best.${ident}ctm.sys | grep 'MS\|FS\|MT\|FT' | wc -l) -gt 0 ] && local/split_results.sh $result/1Best.${ident}ctm.sys $result/1Best.${ident}ctm.sys.split
 		done
 	done
+
+	#add an output variant without the scores
+	for iac in $inv_acoustic_scale; do
+		for wip in $word_ins_penalty; do
+			ident=
+			[ $mwip ] && ident="$wip."
+			[ $miac ] && ident="$ident$iac."
+			cat $result/1Best.${ident}txt | cut -d'(' -f 1 > $inter/1Best_plain.${ident}txt
+		done
+	done
 fi
 
-if [ ! -f $results/1Best.{$ident}ctm ]; then
-    die "Expected output file $result/1Best.${ident}ctm not found after decoding!"
-else
-    echo "Output written to $result/1Best.${ident}ctm and $result/1Best.${ident}txt" >&2
+if [ -z "$mwip" ] && [ -z "$miac" ]; then
+	#these parts of the pipeline only work when idents are not used (mwip/miac are unset)
+
+	if [ ! -f $results/1Best.ctm ]; then
+		die "Expected output file $result/1Best.ctm not found after decoding!"
+	fi
+
+	## convert to XML if output exists
+	if [ $stage -le 10 ] && [ -s $result/1Best.ctm ] && [ -x ./scripts/ctm2xml.py ];
+	then
+		echo -e "\n==========================">&2
+		echo -e "Conversion to XML" | tee $inter/stage >&2
+		echo "==========================">&2
+
+		./scripts/ctm2xml.py "$result" "1Best" "$inter" || die "ctm2xml failed"
+	fi
+
+	## process speaker diarisation output
+	if [ $stage -le 11 ] && [ -s $result/liumlog/1Best.seg ] && [ -x ./scripts/addspkctm.py ];
+	then
+		echo -e "\n==========================">&2
+		echo -e "Processing speaker diarisation output" | tee $inter/stage >&2
+		echo "==========================">&2
+
+		#note, idents (mwip/miac are not for pipelines that use this and late stages
+
+		# Create .rttm
+		spkr_seg="$result/liumlog/1Best.seg"
+		cat "$spkr_seg" | sed -n '/;;/!p' | sort -nk3 | awk '{printf "SPEAKER %s %s %.2f %.2f <NA> <NA> %s <NA>\n", $1, $2, ($3 / 100), ($4 / 100), $8}' > "$result/1Best.rttm" || die "Failure creating RTTM file from speaker diarisation output"
+
+		# Create .ctm with speaker ids
+		./scripts/addspkctm.py "$result/1Best.rttm" "$result/1Best.ctm" || die "Failure adding speakers to CTM"
+	fi
+
+	if [ $stage -le 12 ] && [ -s $result/liumlog/1Best.seg ] && [ -x ./scripts/wordpausestatistic.perl ]; then
+		echo -e "\n==========================">&2
+		echo -e "Adding sentence boundaries" | tee $inter/stage >&2
+		echo "==========================">&2
+
+		# Add sentence boundaries
+		cat $result/1Best.ctm | perl scripts/wordpausestatistic.perl 1.0 "$result/1Best.sent" || die "Failure adding sentence boundaries"
+	fi
+
 fi
+
+echo "Output written to:">&2
+echo " - CTM:                 $result/1Best.ctm">&2
+echo " - Text with scores:    $result/1Best.txt">&2
+echo " - Text without scores: $result/1Best_plain.txt" >&2
+echo " - XML:                 $result/1Best.xml" >&2
 echo "Done" | tee $inter/stage >&2
